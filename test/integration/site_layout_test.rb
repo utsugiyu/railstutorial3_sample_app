ad17d7957b7e4cd764c5f857863964263a4aa4d2
Yu Utsugi <utsugiu@gmail.com>
2019-04-30 00:10:43 +0900

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -12,5 +16,27 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title('Sign up')
+  end
+
+  test "layout links before login" do
+    get root_path
+    assert_select "a[href=?]", root_path, count:2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+
+  test "layout links after login" do
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count:2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -53,4 +53,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     log_in_as(@user, remember_me: '0')
     assert_empty cookies['remember_token']
   end
+
+
 end
